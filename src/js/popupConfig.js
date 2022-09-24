4ce8582a631c3fd3724c71fcf06c38f4d5c0bc02
hxxtae <fkdlxmfkdl@gmail.com>
2022-09-24 20:10:48 +0900

    :books: [Dcos]: Update popupConfig.js
    update image file name by build

--- src/js/popupConfig.js
+++ src/js/popupConfig.js
@@ -72,36 +72,36 @@ const imgDevName = {
 // Build
 const imgBuildName = {
   LOGIN: {
-    BEFORE: 'mw_address.131beb94.PNG',
-    AFTER: 'we_login.55d31020.PNG',
+    BEFORE: 'mw_main.aedf805e.PNG',
+    AFTER: 'we_login.7e2fd9d0.PNG',
   },
   MAIN: {
-    BEFORE: 'mw_main.0d904070.PNG',
-    AFTER: 'we_main.560d9340.PNG',
+    BEFORE: 'mw_main.aedf805e.PNG',
+    AFTER: 'we_main.e8f08d9d.PNG',
   },
   ADDRESS: {
-    BEFORE: 'mw_address.76d49ea8.PNG',
-    AFTER: 'we_address.a1b07bd7.PNG',
+    BEFORE: 'mw_address.131beb94.PNG',
+    AFTER: 'we_address.eca32eb5.PNG',
   },
   CUSTOMER: {
-    BEFORE: 'mw_customer.b0089598.PNG',
-    AFTER: 'we_customer.975c1046.PNG',
+    BEFORE: 'mw_customer.d62d0a76.PNG',
+    AFTER: 'we_customer.9a89caf1.PNG',
   },
   PRODUCTS: {
-    BEFORE: 'mw_products.d6edd1f2.PNG',
-    AFTER: 'we_products.674e7d83.PNG',
+    BEFORE: 'mw_products.df55029b.PNG',
+    AFTER: 'we_products.c684c2ea.PNG',
   },
   ORDER: {
-    BEFORE: 'mw_order.e9be7c4b.PNG',
-    AFTER: 'we_record.61d3181c.PNG',
+    BEFORE: 'mw_order.d5c5e74a.PNG',
+    AFTER: 'we_record.0e32baf9.PNG',
   },
   HISTORY: {
-    BEFORE: 'mw_history.04caf80e.PNG',
-    AFTER: 'we_history.966db0ae.PNG',
+    BEFORE: 'mw_history.3f67c26d.PNG',
+    AFTER: 'we_history.560572dc.PNG',
   },
   SALE: {
-    BEFORE: 'mw_sales.fe4712aa.PNG',
-    AFTER: 'we_sales.9a8f0865.PNG',
+    BEFORE: 'mw_sales.21efd559.PNG',
+    AFTER: 'we_sales.c6e9af81.PNG',
   }
 }
 
